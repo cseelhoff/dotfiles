cc2b513259522e1ea57641703630084be725e46c
Caleb Seelhoff <cseelhoff@gmail.com>
2024-09-24 08:47:51 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -90,7 +90,16 @@
                 };
               };
               users.defaultUserShell = pkgs.zsh;
-              virtualisation.podman.enable = true;
+              virtualisation = {
+                podman = {
+                enable = true;
+
+                # Create a `docker` alias for podman, to use it as a drop-in replacement
+                dockerCompat = true;
+
+                # Required for containers under podman-compose to be able to talk to each other.
+                defaultNetwork.settings.dns_enabled = true;
+              };
             })
           ];
         };
